08305f2971574304d73a45830ba866e9280248b9
DOOM\Yang <13390560336@163.com>
2018-06-28 13:57:34 +0800

    添加ignore与add colorStream

--- heresy/main.cpp
+++ heresy/main.cpp
@@ -18,24 +18,38 @@ int main(int argc, char** argv)
 	streamDepth.create(devAnyDevice, openni::SENSOR_DEPTH);
 	streamDepth.start();
 
+	// 4a. create color stream
+	openni::VideoStream streamColor;
+	streamColor.create(devAnyDevice, openni::SENSOR_COLOR);
+	streamColor.start();
+
 	// 5 main loop, continue read
 	openni::VideoFrameRef frameDepth;
+	openni::VideoFrameRef frameColor;
 	for (int i = 0; i < 100; ++i)
 	{
 		// 5.1 get frame
 		streamDepth.readFrame(&frameDepth);
+		streamColor.readFrame(&frameColor);
 
 		// 5.2 get data array
 		const openni::DepthPixel* pDepth
 			= (const openni::DepthPixel*)frameDepth.getData();
+		const openni::RGB888Pixel* pColor
+			= (const openni::RGB888Pixel*)frameColor.getData();
 
 		// 5.3 output the depth value of center point
 		int idx = frameDepth.getWidth() * (frameDepth.getHeight() + 1) / 2;
-		std::cout << pDepth[idx] << std::endl;
+		std::cout << pDepth[idx] << "( "
+			<< (int)pColor[idx].r << ","
+			<< (int)pColor[idx].g << ","
+			<< (int)pColor[idx].b << ")"
+			<< std::endl;
 	}
 
 	// 6. close
 	streamDepth.destroy();
+	streamColor.destroy();
 	devAnyDevice.close();
 
 	// 7. shutdown
