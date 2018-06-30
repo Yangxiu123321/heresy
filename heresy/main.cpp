f453692ed5597efab9df7adb92d12c3e7bd65ca4
DOOM\Yang <13390560336@163.com>
2018-06-30 08:32:13 +0800

    设备连接监听事件(未成功)

--- heresy/main.cpp
+++ heresy/main.cpp
@@ -12,32 +12,20 @@ using namespace openni;
 /**
 * The listener for depth video stream
 */
-class OutputDpeth : public VideoStream::NewFrameListener
+class GetCamera : public OpenNI::DeviceConnectedListener
 {
 public:
-	// this function will be called when video stream get new frame
-	void onNewFrame(VideoStream& rStream)
+
+	void onDeviceConnected(const DeviceInfo* rDevInfo)
 	{
-		VideoFrameRef vfDepth;
-		if (rStream.readFrame(&vfDepth) == STATUS_OK)
+		Device orbbec;
+		if (orbbec.open(rDevInfo->getUri()) == STATUS_OK)
 		{
-			// a1 get data array
-			const DepthPixel* pDepth
-				= static_cast<const DepthPixel*>(vfDepth.getData());
-
-			// a2 output the depth value of center point
-			int w = vfDepth.getWidth(),
-				h = vfDepth.getHeight();
-			int x = w / 2, y = h / 2;
-			cout << pDepth[x + y * w] << endl;
-
-			// a3 release frame
-			vfDepth.release();
+			std::cout << "open ok" << std::endl;
 		}
 		else
 		{
-			cerr << "Can not read frame\n";
-			cerr << OpenNI::getExtendedError() << endl;
+			std::cerr << OpenNI::getExtendedError() << std::endl;
 		}
 	}
 };
@@ -47,36 +35,19 @@ int main(int argc, char** argv)
 	// 2. initialize OpenNI
 	OpenNI::initialize();
 
-	// 3. open a device
-	Device devDevice;
-	devDevice.open(ANY_DEVICE);
+	GetCamera getOrbbec;
 
-	// 4. create depth stream
-	VideoStream vsDepth;
-	vsDepth.create(devDevice, SENSOR_DEPTH);
-
-	OutputDpeth mOutputDepth;
-
-	vsDepth.addNewFrameListener(&mOutputDepth);
+	OpenNI::addDeviceConnectedListener(&getOrbbec);
 	
-	vsDepth.start();
 
 	// 5 wait any input (some char and Enter) to quit
 	string sInput;
 	cin >> sInput;
 
-	// 6 stop reading
-	vsDepth.removeNewFrameListener(&mOutputDepth);
-	vsDepth.stop();
-
-	// destroy depth stream
-	vsDepth.destroy();
-
-	// close device
-	devDevice.close();
-
 	// 7. shutdown
 	OpenNI::shutdown();
 
 	return 0;
+
+	
 }
